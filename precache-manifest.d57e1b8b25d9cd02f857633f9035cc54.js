b7fae1ac0e9869cd8d388a1d40a3953dd74d9e70
dianmjs <dianamjs69@gmail.com>
2020-05-29 14:42:44 -0500

    Updates

---
+++ precache-manifest.d57e1b8b25d9cd02f857633f9035cc54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ea255646cda5a056c3f22916c75b23",
+    "url": "/posts/index.html"
+  },
+  {
+    "revision": "f883a2f8b01417b999fc",
+    "url": "/posts/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "399f53695386e95fc044",
+    "url": "/posts/static/js/2.b54d1a5d.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/posts/static/js/2.b54d1a5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f883a2f8b01417b999fc",
+    "url": "/posts/static/js/main.25294c20.chunk.js"
+  },
+  {
+    "revision": "22f918f354388f1016d0",
+    "url": "/posts/static/js/runtime-main.d0d3be99.js"
+  }
+]);
